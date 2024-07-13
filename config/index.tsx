d7f4bc1c728d494fb71b260f58875d48fa30ddce
Michael Cunningham <mcunni117@gmail.com>
2024-07-13 19:43:24 -0400

    boilerplate nextjs/wagmi/walletconnect-appkit

---
+++ config/index.tsx
@@ -0,0 +1,28 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
+import { mainnet, polygon } from 'wagmi/chains'
+
+// Get projectId from https://cloud.walletconnect.com
+export const projectId = process.env.NEXT_PUBLIC_W3C_PID
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+const metadata = {
+  name: 'io.vault VS dApp',
+  description: 'manage whitelisted addresses for your io.vault Virtual Signer',
+  url: 'http://localhost:3000/', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [mainnet, polygon] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+})
